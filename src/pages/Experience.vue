a21ef6c603f39543d2c5390adab8f42b1ae82731
hasan <hassan.lookzade@gmail.com>
2022-09-14 18:10:36 +0430

    Experience.vue done bug fix reamain

--- src/pages/Experience.vue
+++ src/pages/Experience.vue
@@ -3,17 +3,17 @@
     <h2 class="justify-center flex q-mb-md " style="color:#9c27b0">Experience</h2>
     <div class="row experienceContainer">
       <div class="col-5  experience ">
-        <p class="justify-center flex text-red-5">Working Experience</p>
+        <p class="justify-center flex text-purple-1">Working Experience :</p>
         <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12  flex ">
-          <q-card class="my-card" flat bordered>
+          <q-card class="experienceCard bg-amber-8" flat bordered>
             <q-card-section>
               <div class="row">
                 <div class="col-lg-7 col-md-6 col-xs-12 col-sm-12">
                   <q-card-section class="q-py-xs">
                     <div class="text-h5 q-mt-sm q-mb-xs">
-                      <a class="project_link" style="text-decoration: none;color: #eb4f4f;">
-                        Junior Front-End at <a href="https://alaatv.com" target="_blank">AlaaTV</a></a>
-                      <p class="text-subtitle2">2021 - 2022</p>
+                      <a class="project_link text-orange-1" style="text-decoration: none;color: #eb4f4f;">
+                        Junior Front-End at <a href="https://alaatv.com" target="_blank" class="text-purple-8">AlaaTV</a></a>
+                      <p class="text-subtitle2 text-purple-8">2021 - 2022</p>
                     </div>
                     <div class="text-caption text-grey-9">
                       In this company we used 3 of vue.js frameworks named Quasar framework , Nuxt.js and Vutify
@@ -33,7 +33,7 @@
                     <q-img
                       width="100%"
                       href="google.com"
-                      class="rounded-borders"
+                      class="rounded-borders bg-white"
                       style="border:1px solid darkgrey"
                       fit="fill"
                       src="https://nodes.alaatv.com/upload/footer-alaaLogo.png?w=45&h=60"
@@ -64,9 +64,6 @@ export default {
     justify-content: center;
     .experience{
       font-size: 28px;
-      .my-card{
-        background-color: #ffd68c;
-      }
     }
   }
 
