c28cb09c07926aabdb2d2debce2e979db17a0f49
hasan <hassan.lookzade@gmail.com>
2022-09-15 19:06:37 +0430

    Experience.vue completed

--- src/pages/Experience.vue
+++ src/pages/Experience.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="exp">
-    <h2 class="justify-center flex q-mb-md " style="color:#9c27b0">Experience</h2>
     <div class="row experienceContainer">
       <div class="col-5  experience ">
-        <p class="justify-center flex text-purple-1">Working Experience :</p>
+        <p class="justify-center flex text-purple-1 text-bold">Working Experience :</p>
         <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12  flex ">
           <q-card class="experienceCard bg-amber-8" flat bordered>
             <q-card-section>
               <div class="row">
                 <div class="col-lg-7 col-md-6 col-xs-12 col-sm-12">
-                  <q-card-section class="q-py-xs">
+                  <q-card-section class="q-py-xs text-bold">
                     <div class="text-h5 q-mt-sm q-mb-xs">
-                      <a class="project_link text-orange-1" style="text-decoration: none;color: #eb4f4f;">
+                      <a class="project_link text-orange-1 text-bold" style="text-decoration: none;color: #eb4f4f;">
                         Junior Front-End at <a href="https://alaatv.com" target="_blank" class="text-purple-8">AlaaTV</a></a>
                       <p class="text-subtitle2 text-purple-8">2021 - 2022</p>
                     </div>
-                    <div class="text-caption text-grey-9">
-                      In this company we used 3 of vue.js frameworks named Quasar framework , Nuxt.js and Vutify
-                      and also we coded pure html css and js in laravel
+                    <div class="text-grey-9" style="font-size: 18px">
+                      In this company we used 3 frameworks of Vue.js named Quasar framework, Nuxt.js and Vuetify
+                      and also we used pure html, css and js code
                       <ul>
-                        <li>Modern and responsive design</li>
-                        <li>Building design based on CRM</li>
-                        <li>Simple CRM Dashboard</li>
-                        <li>Activity tracking on Calendar</li>
-                        <li>Export feature</li>
+                        <li><a href="https://alaatv.com/landing/9"> Landing for each new products</a></li>
+                        <li><a href="https://alaatv.com/map"> site map page</a></li>
+                        <li><a href="https://alaatv.com/t#/"> Ticketing panel</a></li>
+                        <li><a href="https://3a.alaatv.com"> Online test system</a></li>
+                        <li><a href="https://soalaa.com/login"> Online test Question Bank</a></li>
+                        <li><a href="https://soalaa.com/login"> Online test breadcrumbs</a></li>
                       </ul>
                     </div>
                   </q-card-section>
@@ -31,7 +31,8 @@
                 <div class="col-lg-5 col-md-4 col-xs-12 col-sm-12">
                   <q-card-section class="col-3 flex ">
                     <q-img
-                      width="100%"
+                      width="250px"
+                      height="300px"
                       href="google.com"
                       class="rounded-borders bg-white"
                       style="border:1px solid darkgrey"
@@ -59,11 +60,18 @@ export default {
 
 <style scoped lang="scss">
 .exp{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
   .experienceContainer{
     display: flex;
     justify-content: center;
     .experience{
       font-size: 28px;
+      .experienceCard{
+        border-radius: 10%;
+      }
     }
   }
 
